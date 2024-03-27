6ca94ea894971e857879d836d96add12424a9123
alamin <alaminakash9754@gmail.com>
2024-03-27 14:04:26 +0600

    added section title

--- src/components/sectionTitle/SectionTitle.jsx
+++ src/components/sectionTitle/SectionTitle.jsx
@@ -2,8 +2,8 @@
 const SectionTitle = ({heading, subHeading}) => {
     return (
         <div className="text-center w-4/12 mx-auto my-6">
-            <p className="italic text-yellow-600 mb-2">--{subHeading}--</p>
-            <h2 className="uppercase text-3xl border-y-4 py-3">{heading}</h2>
+            <p className="italic text-purple-600 mb-2 ">--{subHeading}--</p>
+            <h2 className="uppercase text-3xl border-y-4 border-y-purple-400 py-3">{heading}</h2>
         </div>
     );
 };

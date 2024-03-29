1789c21a482dbd6b36830ed10ac775c72c024780
alamin <alaminakash9754@gmail.com>
2024-03-29 19:39:44 +0600

    each product can details

--- src/Pages/showAllProducts/EachProductDetails.jsx
+++ src/Pages/showAllProducts/EachProductDetails.jsx
@@ -1,16 +1,34 @@
-import {  useLoaderData } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
+import {  useLoaderData, useParams } from "react-router-dom";
 
 
 const EachProductDetails = () => {
+
     const products = useLoaderData();
-    console.log(products);
+    // const {id} = useParams();
+    // console.log(id);
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await axios.get(`http://localhost:5000/products/${id}`);
+    //             console.log(response.data);
+    //         } catch (error) {
+    //             console.error('Error fetching product details:', error);
+    //         }
+    //     };
+    
+    //     fetchData();
+    // }, [id]);
+    // console.log(products);
     const {name, title} = products;
     
 
     
     return (
         <div>
-            <h2>Name: {name}</h2>
+            <h2>Name:{name}</h2>
         </div>
     );
 };

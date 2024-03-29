9235d7d22255606b705c7c4d5b4501198b1ab5d3
alamin <alaminakash9754@gmail.com>
2024-03-30 02:02:02 +0600

    changes in user home

--- src/Pages/Dashboard/userHome/UserHome.jsx
+++ src/Pages/Dashboard/userHome/UserHome.jsx
@@ -1,15 +1,18 @@
 import useAuth from "../../../Hooks/useAuth";
 
 const UserHome = () => {
-    const {user } = useAuth()
+    const { user } = useAuth()
     return (
-        <div>
+        <div className="flex justify-evenly">
             <h2 className="text-3xl">
                 <span>Hi, Welcome  </span>
                 {
                     user?.displayName ? user.displayName : 'Back'
                 }
             </h2>
+            <h1>
+                <img className="w-20 h-20 rounded-3xl mr-2" src={user.photoURL} />
+            </h1>
         </div>
     );
 };
